4645a78487e6e381d7eef57cd73420aa1ae57880
xueguichen <long1234@MG>
2024-05-16 18:52:42 +0800

    最小的k个数

---
+++ src/main/java/org/example/剑指/最小的k个数/Solution.java
@@ -0,0 +1,37 @@
+package org.example.剑指.最小的k个数;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] smallestK(int[] arr, int k) {
+        if(k == 0 ){
+            return new int[]{};
+        }
+        if( k < 0 || arr.length < k ){
+            return null;
+        }
+        //大顶堆
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k, (num1, num2) -> num2 - num1);
+        for (int i = 0; i < k; i++) {
+            queue.add(arr[i]);
+        }
+        for (int i = k; i < arr.length; i++) {
+            Integer peek = queue.peek();
+            if(arr[i] < peek){
+                queue.poll();
+                queue.add(arr[i]);
+            }
+        }
+
+        return queue.stream().mapToInt(v-> v).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,5,7,2,4,6,8};
+        int k = 4;
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.smallestK(arr, k)));
+
+    }
+}
