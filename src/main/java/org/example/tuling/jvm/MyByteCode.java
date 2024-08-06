fcb56144566aaa05319af11951e455c40dae27f9
xueguichen <long1234@MG>
2024-08-06 19:19:48 +0800

    jvm内存模型

---
+++ src/main/java/org/example/tuling/jvm/MyByteCode.java
@@ -0,0 +1,25 @@
+package org.example.tuling.jvm;
+
+/**
+ * @author chenxuegui
+ * @since 2024/8/6
+ */
+public class MyByteCode {
+
+    private int i = 0;
+
+    private User user;
+
+    private static  User empty = new User();
+
+    private String abc = "dhisvfapdbdvndkhnabjbaj1111111111111111111111111111sasasas";
+
+
+    public void updateUserName(){
+        String name = "ahahahahha";
+    }
+
+    public void updateUser(User user){
+        String name = new String("longma");
+    }
+}
