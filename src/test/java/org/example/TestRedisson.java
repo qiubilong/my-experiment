da859b537e632cf239c07adf2b0661d4431c50ac
xueguichen <long1234@MG>
2025-02-11 17:45:40 +0800

    redisson

--- src/test/java/org/example/TestRedisson.java
+++ src/test/java/org/example/TestRedisson.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +20,40 @@ public class TestRedisson {
 
 
     @Test
-    public void 重入锁源码(){
+    public void 重入锁源码() throws Exception{
+
+
+        Thread.sleep(3* 1000);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doMain();
+            }
+        }).start();
+
+        Thread.sleep(10* 1000);//等待获取锁成功
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doMain();
+            }
+        }).start();
+        Thread.sleep(300* 1000);
+
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doMain();
+            }
+        }).start();
+        Thread.sleep(10* 1000);
+
+
+        Thread.sleep(300000* 1000);
 
     }
 
@@ -31,7 +64,7 @@ public class TestRedisson {
         lock.lock();
         try {
            log.info("获得锁");
-            Thread.sleep(30* 1000);
+            Thread.sleep(300* 1000);
         }catch (Exception e){
             e.printStackTrace();
         }finally {
